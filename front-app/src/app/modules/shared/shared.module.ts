0be20efdd450e11e4e5b13faccea1d39eba1615f
will-cesar <willcesar010421@gmail.com>
2021-09-07 17:20:31 -0300

    Add do componente de pesquisa no modulo shared

--- front-app/src/app/modules/shared/shared.module.ts
+++ front-app/src/app/modules/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, InputSearchComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, InputSearchComponent],
 })
 export class SharedModule {}
