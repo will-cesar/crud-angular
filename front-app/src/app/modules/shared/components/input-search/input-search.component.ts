188443be548243311286643991349d616ae16a31
will-cesar <willcesar010421@gmail.com>
2021-09-07 17:49:13 -0300

    configuração do componente search input e add do icon de clean na app

--- front-app/src/app/modules/shared/components/input-search/input-search.component.ts
+++ front-app/src/app/modules/shared/components/input-search/input-search.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'input-search',
@@ -6,7 +15,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-search.component.scss'],
 })
 export class InputSearchComponent implements OnInit {
+  @ViewChild('fieldSearch', { static: true }) fieldSearch: ElementRef;
+  @Output() inputValue = new EventEmitter<string>();
+
+  inputSearchValue: any = '';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  private sendValue(): void {
+    this.inputValue.emit(this.inputSearchValue);
+  }
+
+  cleanInputSearch(): void {
+    this.inputSearchValue = '';
+  }
+
+  ngOnInit(): void {
+    this.setEventToInput();
+  }
+
+  setEventToInput(): void {
+    fromEvent(this.fieldSearch.nativeElement, 'keyup')
+      .pipe(
+        map((event: any) => {
+          return event.target.value;
+        }),
+        debounceTime(1000),
+        distinctUntilChanged()
+      )
+      .subscribe((text: string) => {
+        this.sendValue();
+      });
+  }
 }
