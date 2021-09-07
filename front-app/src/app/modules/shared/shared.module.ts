3fbdc6dba9c9f43cf7394c6586b722219558dc06
will-cesar <willcesar010421@gmail.com>
2021-09-07 17:45:56 -0300

    add formModule no shared module

--- front-app/src/app/modules/shared/shared.module.ts
+++ front-app/src/app/modules/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InputSearchComponent } from './components/input-search/input-search.component';
 
 @NgModule({
   declarations: [NavbarComponent, InputSearchComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   exports: [NavbarComponent, InputSearchComponent],
 })
 export class SharedModule {}
