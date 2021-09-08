0b3de529978b04d9659736bb2b5d5f7f4e5e30ba
will-cesar <willcesar010421@gmail.com>
2021-09-07 21:15:59 -0300

    finalização da listagem mockada com paginação

--- front-app/src/app/modules/home/pages/list-clients/list-clients.component.ts
+++ front-app/src/app/modules/home/pages/list-clients/list-clients.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { clientsMock } from '../../mocks/clients.mock';
 
 @Component({
   selector: 'app-list-clients',
   templateUrl: './list-clients.component.html',
-  styleUrls: ['./list-clients.component.scss']
+  styleUrls: ['./list-clients.component.scss'],
 })
 export class ListClientsComponent implements OnInit {
+  isLoading: boolean = false;
+  listOfClients: any[] = clientsMock;
+  paginationCurrentPage: number = 1;
+  paginationItemsPerPage: number = 10;
+  totalOfClients: number = clientsMock.length;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  deleteClient(): void {}
+
+  pageChanged(page: number): void {
+    this.paginationCurrentPage = page;
   }
 
+  test(inputValue: string): void {
+    console.log(inputValue);
+  }
 }
